dc5a69eb3143fe9c31b7c55b0d1389bea1cb5306
Patrick Tjin <pattjin@google.com>
2021-02-04 06:11:00 +0545

    gauguin: Remove omx from _64 builds.
    Omx is 32-bit only.
    
    Bug: 179159233
    Signed-off-by: Pat Tjin <pattjin@google.com>
    Change-Id: Ic2382db78da15187e36a8881c79ed70a8ee78d54

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
 # HIDL VINTF Manifsets
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest_omx.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += nfc
