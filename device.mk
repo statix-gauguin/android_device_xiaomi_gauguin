46c2daf7b6f48391d313280c423ba01b61661638
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:06:43 +0100

    gauguin: Build RIL libs
    Change-Id: I449475abffebdf837e5645e77b571e0efd9988a1

--- device.mk
+++ device.mk
@@ -146,6 +146,11 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    librmnetctl \
+    libxml2
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
