03505113a2a4ab9511a01f7232f0ab60d2f022b6
Ethan Chen <intervigil@gmail.com>
2021-10-27 20:26:09 +0530

    gauguin: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -284,6 +284,10 @@ PRODUCT_COPY_FILES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
