11a44ecc747089683d509ddf40bdf896d5b361b1
althafvly <althafvly@gmail.com>
2022-11-19 12:45:23 +0530

    gauguin: Add libpiex_shim for com.mi.node.watermark.so
    Change-Id: I9e8b125ab5dfaa325db63b02a7fcf51577ee7b0d

--- device.mk
+++ device.mk
@@ -85,6 +85,9 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libgui_vendor
 
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
