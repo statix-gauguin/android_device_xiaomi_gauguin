5e55cb09d16a47c94ebbdb589eeac44b1d7a7d14
hridaya <hridaya@pixelos.net>
2023-02-19 23:35:53 +0545

    gauguin: Import excluded-input-devices.xml from stock
    Change-Id: I12136e42a28960edd1e8585ae632523f20a1c8b0

--- device.mk
+++ device.mk
@@ -46,5 +46,8 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/gauguin/gauguin-vendor.mk)
