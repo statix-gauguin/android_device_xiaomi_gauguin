7b95114b758febd3ab9811ce6cda0254be59c579
hridaya <hridaya@pixelos.net>
2023-02-22 18:33:35 +0545

    gauguin: Import public.libraries.txt from stock
    Change-Id: I32ac9717cd2c171d6b28b8ea14adbff9cef6d305

--- device.mk
+++ device.mk
@@ -245,6 +245,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Power
 $(call inherit-product, vendor/qcom/opensource/power/power-vendor-product.mk)
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
